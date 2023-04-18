2ae42fe51e7b8918c4930ad46f52fc779b2314b5
nulladdict <nulladdicted@gmail.com>
2023-04-18 13:29:57 +0530

    fix(nvim): add keymap for formatting

--- .config/nvim/lua/plugins/null_ls.lua
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -16,15 +16,20 @@ null_ls.setup({
     },
     on_attach = function(_, bufnr)
         -- Create a command `:Format` local to the LSP buffer
+        local function format()
+            vim.lsp.buf.format({
+                filter = function(client)
+                    return client.name == 'null-ls'
+                end,
+                bufnr = bufnr
+            })
+        end
+        vim.keymap.set({'n', 'v'}, '<C-p>', format,
+                       {noremap = true, silent = true})
         for _, fmt_command in ipairs({'Format', 'Fmt'}) do
-            vim.api.nvim_buf_create_user_command(bufnr, fmt_command, function()
-                vim.lsp.buf.format({
-                    filter = function(client)
-                        return client.name == 'null-ls'
-                    end,
-                    bufnr = bufnr
-                })
-            end, {desc = 'Format current buffer with LSP'})
+            vim.api.nvim_buf_create_user_command(bufnr, fmt_command, format, {
+                desc = 'Format current buffer with LSP'
+            })
         end
     end
 })
