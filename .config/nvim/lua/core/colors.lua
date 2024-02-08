7b6437683baee1cc64c6f931ee6afbb7ce8a72c2
nulladdict <nulladdicted@gmail.com>
2024-02-08 18:34:30 +0800

    fix(nvim): remove bg from more groups

--- .config/nvim/lua/core/colors.lua
+++ .config/nvim/lua/core/colors.lua
@@ -1,5 +1,12 @@
 require('rose-pine').setup({
-    styles = { italic = false, transparency = true, },
+    styles = { italic = false, transparency = true },
+    before_highlight = function(group, highlight)
+        local no_bg = { 'MatchParen', "@comment.todo", "@comment.hint", "@comment.info", "@comment.note" }
+        if vim.tbl_contains(no_bg, group) then
+            highlight.bg = nil
+            highlight.blend = nil
+        end
+    end,
 })
 vim.api.nvim_create_autocmd('ColorScheme', { pattern = '*', command = 'hi! link Sneak Search' })
 vim.opt.termguicolors = true
