66cec46404ed31f076adb88cf8194f76305a4c8a
nulladdict <nulladdicted@gmail.com>
2024-07-20 19:12:06 +0700

    fix(nvim): udpate plugins

--- .config/nvim/lua/plugins/gitsings.lua
+++ .config/nvim/lua/plugins/gitsings.lua
@@ -6,7 +6,6 @@ require('gitsigns').setup {
         topdelete = { text = '‾' },
         changedelete = { text = '~' }
     },
-    _signs_staged_enable = true,
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
