53c5db534fb96a789384c9ba7c3fd47ed00e9eea
nulladdict <nulladdicted@gmail.com>
2025-07-22 19:30:55 +0200

    feat(nix): add flake.nix backup

---
+++ darwin-flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # Using Determinate Nix
+      nix.enable = false;
+
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        git
+        git-lfs
+        lazygit
+        gh
+
+        neovim
+        vscode
+
+        fzf
+        ripgrep
+        fd
+        jq
+        tmux
+        curl
+        htop
+
+        nodejs_latest
+        yarn
+      ];
+
+      homebrew = {
+        enable = true;
+        taps = [
+          "daipeihust/tap"
+          "nikitabobko/tap"
+        ];
+        brews = [
+          "im-select"
+        ];
+        casks = [
+          "brave-browser"
+          "betterdisplay"
+          "aerospace"
+          "ghostty"
+          "telegram"
+          "mattermost"
+          "figma"
+        ];
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      system.primaryUser = "nulladdict";
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mini
+    darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
