1c5edf3f922d01fff24edeb960ef3ca93ca0341d
nulladdict <nulladdicted@gmail.com>
2026-06-23 18:24:25 +0200

    fix(nvim): update plugins

--- .config/nvim/plugin/05-completion.lua
+++ .config/nvim/plugin/05-completion.lua
@@ -51,29 +51,21 @@ end
 vim.pack.add({ 'https://github.com/github/copilot.vim' })
 do
     vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
-    vim.g.copilot_no_tab_map = true
     vim.g.copilot_version = 'latest'
 
-    local function appy_suggestion(fallback)
-        return function()
-            if require('sidekick').nes_jump_or_apply() then
-                return
-            end
-            local suggestion = vim.fn['copilot#GetDisplayedSuggestion']()
-            if suggestion.text ~= nil and suggestion.text ~= '' then
-                return vim.fn['copilot#Accept'](fallback)
-            end
-            return fallback
+    local function appy_suggestion()
+        local suggestion = vim.fn['copilot#GetDisplayedSuggestion']()
+        if suggestion.text ~= nil and suggestion.text ~= '' then
+            return vim.fn['copilot#Accept']('')
         end
+        return ''
     end
     local function clear_suggestion()
         vim.fn['copilot#Dismiss']()
-        require('sidekick.nes').clear()
     end
 
-    for key, fallback in pairs({ ['<tab>'] = '\t', ['<D-j>'] = '', ['<D-о>'] = '' }) do
-        vim.keymap.set({ 'i', 'n' }, key, appy_suggestion(fallback), { expr = true, replace_keycodes = false })
-    end
+    vim.keymap.set({ 'i', 'n' }, '<D-j>', appy_suggestion, { expr = true, replace_keycodes = false })
+    vim.keymap.set({ 'i', 'n' }, '<D-о>', appy_suggestion, { expr = true, replace_keycodes = false })
     vim.keymap.set({ 'i', 'n' }, '<D-l>', clear_suggestion, { silent = true })
     vim.keymap.set({ 'i', 'n' }, '<D-д>', clear_suggestion, { silent = true })
 
@@ -83,9 +75,4 @@ do
             vim.b.copilot_enabled = false
         end,
     })
-end
-
-vim.pack.add({ 'https://github.com/folke/sidekick.nvim' })
-do
-    require('sidekick').setup()
 end
